efafbb63603b81b0ae4ec11ad974515f1da9fa3b
Jennifer Luk <jennifer.luk21@ycdsbk12.ca>
2020-11-25 22:27:38 +0000

    took out jen's methods

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -2,27 +2,4 @@ package gr11review.part2;
 
 public class Utility{
 
-  public static int sumNumbers(String str) {
-
-    // declare variables
-    int numbersum = 0;
-    
-    // 
-    for (int i = 0; i < str.length(); i++) {
-      if (Character.isDigit(str.charAt(i))) {
-        int count = 0; 
-        for (count = i+1; count < str.length() && Character.isDigit(str.charAt(count)); count++) { 
-          if (Character.isDigit(str.charAt(count))) {
-            count++;
-          }else {
-            break;
-          }
-          numbersum = numbersum + Integer.parseInt(str.substring(i, count));
-          i = count;
-        }
-      }
-    }
-    return numbersum; 
-
-  }
 }
