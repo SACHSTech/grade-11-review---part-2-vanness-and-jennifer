f7469ca545850cf0229a24530397fb5db1f9611d
Jennifer Luk <jennifer.luk21@ycdsbk12.ca>
2020-11-25 22:26:38 +0000

    imported methods 2 back to my own branch

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,25 @@
 package gr11review.part2;
 
+import java.io.*;
+
+public class Main{
+
+  public static void main(String[] args) throws IOException{
+  
+  // instantiate BufferedReader object
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+  
+  // declare variables
+  String strInput; 
+  int intsum;
+
+  // ask for string input
+  System.out.print("Enter a string: ");
+  strInput = key.readLine();
+  
+  // print the sum
+  System.out.println("The sum of the digits in the string is: "+Utility.sumNumbers(strInput));
+
+
+  }
+}
