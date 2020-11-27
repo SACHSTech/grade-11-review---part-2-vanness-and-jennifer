143670b1c95c19c6fe3339cebde9f448b1b30aed
Vanness Yang <vanness.yang21@ycdsbk12.ca>
2020-11-27 15:33:26 +0000

    Method 1 Done

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,11 @@
 package gr11review.part2;
 
+import gr11review.part2.Utility.*;
+import java.io.*;
+
+public class Main{
+  public static void main(String[] args) throws IOException{
+    System.out.println(Utility.zipZap("zzipzap"));
+  
+  }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -2,4 +2,42 @@ package gr11review.part2;
 
 public class Utility {
     
+public static String zipZap(String strWord){
+  int intLength;
+  int intCount;
+  String strNoMiddle;
+  String strPFind;
+  String strCharacter;
+  String strMiddleFind; 
+  
+  intLength = strWord.length();
+
+  strNoMiddle = "";
+
+  for(intCount = 0; intCount < (intLength); intCount++){
+    //System.out.println(intCount);
+    strCharacter = strWord.substring(intCount, intCount+1);
+    if(intLength - 2 <= intCount){
+      strNoMiddle = strNoMiddle + strCharacter;
+    }else{
+      strPFind = strWord.substring(intCount+2, intCount+3);
+      strMiddleFind = strWord.substring(intCount+1, intCount+2);
+      if(!strCharacter.equalsIgnoreCase("Z")){
+        strNoMiddle = strNoMiddle + strCharacter;
+      }else if(strCharacter.equalsIgnoreCase("Z") &&  !strPFind.equalsIgnoreCase("P")){
+        strNoMiddle = strNoMiddle + strCharacter;
+      }else if(strCharacter.equalsIgnoreCase("Z") && strPFind.equalsIgnoreCase("P")){
+        //strMiddleFind = " ";
+        strNoMiddle = strNoMiddle + strCharacter + strPFind;
+        if(intLength - 2 <= intCount){
+        }else{
+          intCount = intCount + 2;
+        } 
+        //System.out.println(strNoMiddle);
+      }
+    }
+  }
+  return strNoMiddle;
+  
+  }
 }
