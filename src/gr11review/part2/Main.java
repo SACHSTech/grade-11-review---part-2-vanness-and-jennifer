0864345bd39874dbefc1b8e2aee7c6b3a8f86b48
Jennifer Luk <jennifer.luk21@ycdsbk12.ca>
2020-11-30 15:54:05 +0000

    minor changes to comments and formatting in main

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -5,9 +5,9 @@ import gr11review.part2.Utility.*;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException{
  
-    // Method 2
+    // Testing Method 2
     // declare variables
     String str = "abc123xyz";
     int intsum;
@@ -17,26 +17,26 @@ public class Main {
     // print the sum
     System.out.println("The sum of the digits in the string is: " + intsum);
 
-    // FileIO 2
+    // Testing FileIO 2
     String Output = Utility.alphaWord("src/gr11review/part2/words.txt");
 
     // print results 
     System.out.println("The word that is alphabetically first is " +Output);
 
-    // Arrays 2
+    // Testing Arrays 2
     int[] nums = {1, 2, 3, 2, 5, 2}; 
     int value = 2;
 
     Utility array2 = new Utility();
     System.out.println(java.util.Arrays.toString(array2.notAlone(nums, value)));
     
-    // //Arrays 4
+    // Testing Arrays 4
     int[] list = {1, 1, 1, 2, 1};
 
     Utility array4 = new Utility();
     System.out.println(array4.canBalance(list));
 
-    // Arrays 6 
+    // Testing Arrays 6 
     int n = 9; 
     int MAX = 100; 
     int[][] mat = new int[MAX][MAX];
